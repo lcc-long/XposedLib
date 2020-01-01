e5eb0df7473ce2ea0ddc96b6a1beb55b6e7b1fd5
z.houbin <z.houbin@foxmail.com>
2020-01-01 15:05:49 +0800

    增加截图

--- Lib/src/main/java/z/houbin/xposed/lib/ui/ViewHelper.java
+++ Lib/src/main/java/z/houbin/xposed/lib/ui/ViewHelper.java
@@ -2,6 +2,7 @@ package z.houbin.xposed.lib.ui;
 
 import android.app.Activity;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.graphics.Point;
 import android.os.SystemClock;
 import android.view.MotionEvent;
@@ -11,11 +12,17 @@ import android.view.ViewParent;
 import android.view.Window;
 import android.widget.TextView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 
+import z.houbin.xposed.lib.file.Files;
+import z.houbin.xposed.lib.log.Logs;
 import z.houbin.xposed.lib.shell.Shell;
 import z.houbin.xposed.lib.thread.ThreadPool;
 
@@ -427,20 +434,22 @@ public class ViewHelper {
     public static String getDepthString(View v, int max) {
         LinkedList<String> deepLinked = new LinkedList<>();
         if (v != null) {
-            ViewGroup p = (ViewGroup) v.getParent();
-            deepLinked.add(getDepthFormat(p, v));
-            for (int i = 0; i < max; i++) {
-                if (p == null) {
-                    break;
-                }
-                if (p.getParent() instanceof ViewGroup) {
-                    ViewGroup pp = (ViewGroup) p.getParent();
-                    if (pp != null) {
-                        deepLinked.add(getDepthFormat(pp, p));
+            if (v.getParent() instanceof ViewGroup) {
+                ViewGroup p = (ViewGroup) v.getParent();
+                deepLinked.add(getDepthFormat(p, v));
+                for (int i = 0; i < max; i++) {
+                    if (p == null) {
+                        break;
                     }
-                    p = pp;
-                } else {
-                    break;
+                    if (p.getParent() instanceof ViewGroup) {
+                        ViewGroup pp = (ViewGroup) p.getParent();
+                        if (pp != null) {
+                            deepLinked.add(getDepthFormat(pp, p));
+                        }
+                        p = pp;
+                    } else {
+                        break;
+                    }
                 }
             }
         }
@@ -474,5 +483,80 @@ public class ViewHelper {
             }
         }
         return id;
+    }
+
+    public static void takeScreenShot(Activity activity, String saveFilePath, int options) {
+        if (activity == null) {
+            return;
+        }
+        View view = activity.getWindow().getDecorView();
+        //允许当前窗口保存缓存信息
+        view.setDrawingCacheEnabled(true);
+        view.buildDrawingCache();
+
+        //获取屏幕宽和高
+        int width = getSceenWidth(activity);
+        int height = getSceenHeight(activity);
+
+        Bitmap bitmap = null;
+        try {
+            bitmap = Bitmap.createBitmap(view.getDrawingCache(), 0, 0, width, height);
+        } catch (Exception e) {
+            Logs.e(e);
+        }
+        //销毁缓存信息
+        view.destroyDrawingCache();
+        view.setDrawingCacheEnabled(false);
+
+        if (null != bitmap) {
+            try {
+                compressAndGenImage(bitmap, saveFilePath, options);
+            } catch (IOException e) {
+                Logs.e(e);
+            }
+        }
+    }
+
+    /**
+     * 获取屏幕高度
+     *
+     * @param activity
+     * @return
+     */
+    public static int getSceenHeight(Activity activity) {
+        return activity.getWindowManager().getDefaultDisplay().getHeight();
+    }
+
+    /**
+     * 获取屏幕宽度
+     *
+     * @param activity
+     * @return
+     */
+    public static int getSceenWidth(Activity activity) {
+        return activity.getWindowManager().getDefaultDisplay().getWidth();
+    }
+
+    /**
+     * 带压缩的保存图片
+     *
+     * @param image
+     * @param outPath
+     * @param options 压缩比例
+     * @throws IOException
+     */
+    public static void compressAndGenImage(Bitmap image, String outPath, int options) throws IOException {
+        Logs.e("图片压缩");
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        // scale
+        // Store the bitmap into output stream(no compress)
+        image.compress(Bitmap.CompressFormat.JPEG, options, os);
+
+        // Generate compressed image file
+        Files.makeParent(new File(outPath));
+        FileOutputStream fos = new FileOutputStream(outPath);
+        fos.write(os.toByteArray());
+        fos.flush();
+        fos.close();
     }
 }
