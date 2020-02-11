d50c53ec73f260e6f612c80ea35a4101b5465fef
z.houbin <z.houbin@foxmail.com>
2020-02-11 11:02:02 +0800

    增加XConfig

--- Lib/src/main/java/z/houbin/xposed/lib/config/XConfig.java
+++ Lib/src/main/java/z/houbin/xposed/lib/config/XConfig.java
@@ -1,6 +1,12 @@
 package z.houbin.xposed.lib.config;
 
-import de.robv.android.xposed.XSharedPreferences;
+import android.os.Environment;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.util.Set;
 
 /**
  * XSharedPreferences
@@ -8,21 +14,62 @@ import de.robv.android.xposed.XSharedPreferences;
  * @author z.houbin
  */
 public class XConfig extends BaseConfig {
+    private static File dir = new File(Environment.getExternalStorageDirectory(), "/.xposed.lib/");
     private static XConfig xConfig;
-    private XSharedPreferences xSharedPreferences;
+    private JSONSharedPreferences jsonSharedPreferences;
 
     private XConfig(String packageName) {
-        xSharedPreferences = new XSharedPreferences(packageName, "config");
-        xSharedPreferences.makeWorldReadable();
-        super.sharedPreferences = xSharedPreferences;
+        jsonSharedPreferences = new JSONSharedPreferences(new File(dir, packageName));
+        jsonSharedPreferences.makeWorldReadable();
+        super.sharedPreferences = jsonSharedPreferences;
     }
 
     public static XConfig getInstance(String packageName) {
         if (xConfig == null) {
             xConfig = new XConfig(packageName);
         } else {
-            xConfig.xSharedPreferences.reload();
+            xConfig.jsonSharedPreferences.reload();
         }
         return xConfig;
+    }
+
+    public void put(String key, JSONObject value) {
+        sharedPreferences.edit().putString(key, value.toString()).apply();
+    }
+
+    public void put(String key, JSONArray value) {
+        sharedPreferences.edit().putString(key, value.toString()).apply();
+    }
+
+    public void put(String key, String value) {
+        sharedPreferences.edit().putString(key, value).apply();
+    }
+
+    public void put(String key, int value) {
+        sharedPreferences.edit().putInt(key, value).apply();
+    }
+
+    public void put(String key, long value) {
+        sharedPreferences.edit().putLong(key, value).apply();
+    }
+
+    public void put(String key, float value) {
+        sharedPreferences.edit().putFloat(key, value).apply();
+    }
+
+    public void put(String key, boolean value) {
+        sharedPreferences.edit().putBoolean(key, value).apply();
+    }
+
+    public void remove(String key) {
+        sharedPreferences.edit().remove(key).apply();
+    }
+
+    public void clear() {
+        sharedPreferences.edit().clear().apply();
+    }
+
+    public void put(String key, Set<String> value) {
+        sharedPreferences.edit().putStringSet(key, value).apply();
     }
 }
